89d4ac656f048a10d1add81bcc116ae90e2539e9
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2026-01-31 14:55:37 -0300

    Add fixtures

---
+++ app/bin/DataFixtures/plans.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Enum\EducationalPlanStatusEnum;
+
+return [
+    [
+        'name' => 'Plano Básico',
+        'value' => 99.89,
+        'status' => 'teste',
+    ],
+    [
+        'name' => 'Plano Premium',
+        'value' => 159.59,
+        'status' => EducationalPlanStatusEnum::ACTIVE,
+    ],
+    [
+        'name' => 'Plano de Entrada',
+        'value' => 9.09,
+        'status' => EducationalPlanStatusEnum::CANCELED,
+    ],
+    [
+        'name' => 'Plano Antigo',
+        'value' => 59.90,
+        'status' => EducationalPlanStatusEnum::EXPIRED,
+    ],
+    [
+        'name' => 'Plano Premium Prime',
+        'value' => 398.19,
+        'status' => EducationalPlanStatusEnum::ACTIVE,
+    ],
+];
