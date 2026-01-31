84041fff6ca464c54eda0e944e2fd07d46a0b94f
mateus <mateusfaustino2024@gmail.com>
2026-01-31 10:14:38 -0300

    Feat: criar entidade de professor

---
+++ app/src/Entity/Professor.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Enum\EducationalPlanStatusEnum;
+use DateTime;
+
+class Professor
+{
+    private ?int $id = null;
+
+    private string $name;
+
+    private ?string $cpf = null;
+
+    private string $category;
+
+    private Address $address;
+
+    private array $shifts = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getCpf(): ?string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(?string $cpf): void
+    {
+        $this->cpf = $cpf;
+    }
+
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): void
+    {
+        $this->category = $category;
+    }
+
+    public function getAddress(): Address
+    {
+        return $this->address;
+    }
+
+    public function setAddress(Address $address): void
+    {
+        $this->address = $address;
+    }
+
+    public function getShifts(): array
+    {
+        return $this->shifts;
+    }
+
+    public function setShifts(array $shifts): void
+    {
+        $this->shifts = $shifts;
+    }
+}
